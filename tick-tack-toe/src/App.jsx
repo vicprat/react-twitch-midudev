6eba2430aaa47a61ee42f096827303bfa8aad84d
Victor Prado Trujillo <vicknan0182@gmail.com>
2023-02-06 18:23:24 -0600

    inicializando repo con 2 proyecto terminados, tic tac toe y mouse followe

---
+++ tick-tack-toe/src/App.jsx
@@ -0,0 +1,89 @@
+/* eslint-disable no-unused-expressions */
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+
+function App () {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+  // null es que no hay ganador, false es que hay empate, true hay ganador
+  const [winner, setWinner] = useState(null)
+
+  // Resetear juego
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
+
+  const checkEndGame = (newBoard) => {
+    // Revisa si hay un empate
+    // sino hay mas espcacios vacioas en el tablero
+    return newBoard.every((square) => square !== null)
+  };
+  (square) => square !== null
+
+  const updateBoard = (index) => {
+    // no actualizar tablero si ya existe algo dentro
+    if (board[index] || winner) return
+    // actualizar tablro
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiar turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // Guardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+    // checar si hay un ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) // empate
+    }
+  }
+
+  return (
+    <main className='board'>
+      <h1>Tic tac toe </h1>
+      <button onClick={resetGame}> Comenzar de nuevo</button>
+      <section className='game'>
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          )
+        })}
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} />
+    </main>
+  )
+}
+
+export default App
